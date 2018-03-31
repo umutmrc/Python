3bcfb028c97b45c533adb9b73062829d9a9bc262
Umut Meriç <37014250+umutmrc@users.noreply.github.com>
2018-03-31 15:34:31 +0300

    Add files via upload

---
+++ hafta6.py
@@ -0,0 +1,50 @@
+################################################
+#
+#ilk_sayi = input("İlk Sayı: ")
+#ikinci_sayi = input("İkinci Sayı: ")
+#ilk_sayi=int(ilk_sayi)
+#ikinci_sayi=int(ikinci_sayi)
+#print(ilk_sayi, "/", ikinci_sayi, "=", ilk_sayi/ikinci_sayi)
+#
+################################################
+#
+#ilk_sayi = input("İlk Sayı: ")
+#ikinci_sayi = input("İkinci Sayı: ")
+#try:
+#    sayi1 = int(ilk_sayi)
+#    sayi2 = int(ikinci_sayi)
+#    print(sayi1, "/", sayi2, "=", sayi1/sayi2)
+#except ZeroDivisionError:
+#    print("Bir Sayıyı 0'a Bölemezsiniz!")
+##except ValueError:
+##    print("Lütfen Sadece Sayı Girin!")
+##except:
+##    print("Beklenmeyen bir hata oluştu! ")
+#
+################################################
+#
+#           sayi=3
+#              ***
+#               **
+#                *
+
+
+#sayilar= int(input("Bir Sayı Girin"))
+#a=1;
+#for i in range(sayilar,0,-1):
+#        print(" "*(i-1),"*"*a)
+#        a+=1;
+#
+################################################
+#
+#           sayi=3
+#           *
+#           **
+#           ***
+
+
+#sayilar= int(input("Bir Sayı Girin"))
+#for i in range(0,sayilar):
+#        print("*"*(i+1))
+#
+################################################
